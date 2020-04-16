129ff6237f96678da03947a12459aa569dd13079
Neil Russell <neilrussell6@gmail.com>
2020-04-16 22:27:22 +0200

    test: fix failing tests in Screen modules

--- src/modules/Screen1/Screen1.test.jsx
+++ src/modules/Screen1/Screen1.test.jsx
@@ -1,7 +1,7 @@
 import 'react-native'
 import React from 'react'
 
-import { Screen1 } from './Screen1'
+import Screen1 from './Screen1'
 
 // Note: test renderer must be required after react-native.
 // eslint-disable-next-line import/order
