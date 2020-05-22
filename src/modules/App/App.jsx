973fbabac10f4bd4a5ff6bc5421b219ef8902364
Ryan Vermooten <ryan@Ryans-MacBook-Pro.local>
2020-05-22 11:54:56 +0200

    feat: initial setup of mqtt on rn -- messy

--- src/modules/App/App.jsx
+++ src/modules/App/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 
+import MQTT from 'sp-react-native-mqtt'
+
 import CommonStyles from '../../common/styles'
+
+/* create mqtt client */
+MQTT.createClient({
+  uri: 'mqtt://test.mosquitto.org:1883',
+  clientId: 'your_client_id',
+}).then(function(client) {
+
+  client.on('closed', function() {
+    console.log('mqtt.event.closed')
+  })
+
+  client.on('error', function(msg) {
+    console.log('mqtt.event.error', msg)
+  })
+
+  client.on('message', function(msg) {
+    console.log('mqtt.event.message', msg)
+  })
+
+  client.on('connect', function() {
+    console.log('connected')
+    client.subscribe('/data', 0)
+    client.publish('/data', 'test', 0, false)
+  })
+
+  client.connect()
+}).catch(function(err){
+  console.log(err)
+})
 
 const App = () => (
   <SafeAreaView style={CommonStyles.container}>
